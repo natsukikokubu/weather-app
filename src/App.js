cfc669d6f5cc902fa4c1935ac3547441949eeac7
natsukikokubu <natsukikokubu3@gmail.com>
2023-07-29 22:33:58 +0900

    add-place-names

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Select from "react-select";
+
+const placeNames = [
+  { value: "Tokyo", label: "東京" },
+  { value: "Osaka", label: "大阪" },
+  { value: "Tochigi", label: "栃木" },
+];
+
+function App() {
+  return (
+    <div>
+      <h1>今日のお天気</h1>
+    </div>
+  );
+}
+
+export default App;
